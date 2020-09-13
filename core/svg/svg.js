748562a5bbb6fd8236968749e06480b872b67ca8
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2020-09-13 14:07:05 +0530

    Add new fonts

--- core/svg/svg.js
+++ core/svg/svg.js
@@ -8,15 +8,87 @@ const svg = ({ text1, text2 = '', height = 400, width = 800, font = '' }) => `
 				font-family: 'un-abhaya';
 				src:  url('../assets/fonts/UN-Abhaya.ttf');
 			}
-
+			@font-face {
+				font-family: 'un-alakamanda';
+				src:  url('../assets/fonts/UN-Alakamanda.ttf');
+			}
 			@font-face {
 				font-family: 'un-arundathee';
 				src:  url('../assets/fonts/UN-Arundathee.ttf');
+			}
+			@font-face {
+				font-family: 'un-basuru';
+				src:  url('../assets/fonts/UN-Basuru.ttf');
+			}
+			@font-face {
+				font-family: 'un-bindumathi';
+				src:  url('../assets/fonts/UN-Bindumathi.ttf');
+			}
+			@font-face {
+				font-family: 'un-davasa';
+				src:  url('../assets/fonts/UN-Davasa.ttf');
+			}
+			@font-face {
+				font-family: 'un-derana';
+				src:  url('../assets/fonts/UN-Derana.ttf');
+			}
+			@font-face {
+				font-family: 'un-dharanee';
+				src:  url('../assets/fonts/UN-Dharanee.ttf');
+			}
+			@font-face {
+				font-family: 'un-disapamok';
+				src:  url('../assets/fonts/UN-Disapamok.ttf');
+			}
+			@font-face {
+				font-family: 'un-ganganee';
+				src:  url('../assets/fonts/UN-Ganganee.ttf');
+			}
+			@font-face {
+				font-family: 'un-gemunu';
+				src:  url('../assets/fonts/UN-Gemunu.ttf');
+			}
+			@font-face {
+				font-family: 'un-gurulugomi';
+				src:  url('../assets/fonts/UN-Gurulugomi.ttf');
+			}
+			@font-face {
+				font-family: 'un-imanee';
+				src:  url('../assets/fonts/UN-Imanee.ttf');
+			}
+			@font-face {
+				font-family: 'un-indeewaree';
+				src:  url('../assets/fonts/UN-Indeewaree.ttf');
+			}
+			@font-face {
+				font-family: 'un-isiwara';
+				src:  url('../assets/fonts/UN-Isiwara.ttf');
+			}
+			@font-face {
+				font-family: 'un-malithi';
+				src:  url('../assets/fonts/UN-Malithi.ttf');
+			}
+			@font-face {
+				font-family: 'un-rajantha';
+				src:  url('../assets/fonts/UN-Rajantha.ttf');
+			}
+			@font-face {
+				font-family: 'un-rashmi';
+				src:  url('../assets/fonts/UN-Rashmi.ttf');
+			}
+			@font-face {
+				font-family: 'un-samantha';
+				src:  url('../assets/fonts/UN-Samantha.ttf');
+			}
+			@font-face {
+				font-family: 'un-sandhyanee';
+				src:  url('../assets/fonts/UN-Sandhyanee.ttf');
 			}
 
 			.container {
 				font-family:
 					${font},
+					un-abhaya,
 					system-ui,
 					-apple-system,
 					'Segoe UI',
